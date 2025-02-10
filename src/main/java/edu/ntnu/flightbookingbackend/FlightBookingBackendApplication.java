2023addf1d3f137427948e8f4fb09a1c6726c92d
Mats <>
2025-02-10 09:12:50 +0100

    Set up project with Spring

---
+++ src/main/java/edu/ntnu/flightbookingbackend/FlightBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.ntnu.flightbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlightBookingBackendApplication.class, args);
+  }
+
+}
