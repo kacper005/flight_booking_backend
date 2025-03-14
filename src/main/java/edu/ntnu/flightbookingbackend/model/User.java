4963bcf7d2c4e48d60061cfbabd51019fc32d291
Mats <>
2025-03-14 09:21:40 +0100

    Added fields, tags, getters and setters to User class

--- src/main/java/edu/ntnu/flightbookingbackend/model/User.java
+++ src/main/java/edu/ntnu/flightbookingbackend/model/User.java
@@ -1,4 +1,119 @@
 package edu.ntnu.flightbookingbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ * A user of the system. Entity class.
+ */
+@Entity
 public class User {
+  @Id
+  @GeneratedValue
+  private int userId;
+  private String email;
+  private String password;
+  private int phone;
+  private String firstName;
+  private String lastName;
+  private String dateOfBirth;
+  private String country;
+  private String gender;
+  private String role;
+  private String createdAt;
+
+  // TODO: Create join table for user to booking and feedback
+  // TODO: Create relationship between user and other classes (one-to-many, many-to-many etc.)
+
+  public User() {
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getPhone() {
+    return phone;
+  }
+
+  public void setPhone(int phone) {
+    this.phone = phone;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public String getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(String createdAt) {
+    this.createdAt = createdAt;
+  }
 }
