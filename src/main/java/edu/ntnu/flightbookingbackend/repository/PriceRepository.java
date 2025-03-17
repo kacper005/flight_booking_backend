eeb5bf0db6ef3e6d90461a9352e1c2d1193c46d7
Mats <>
2025-03-17 15:43:17 +0100

    Added PriceRepository

---
+++ src/main/java/edu/ntnu/flightbookingbackend/repository/PriceRepository.java
@@ -0,0 +1,10 @@
+package edu.ntnu.flightbookingbackend.repository;
+
+import edu.ntnu.flightbookingbackend.model.Price;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Repository interface for accessing Price data in the database.
+ */
+public interface PriceRepository extends CrudRepository<Price, Integer> {
+}
