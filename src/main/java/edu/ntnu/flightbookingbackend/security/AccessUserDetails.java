449675f77b497602f8bb7828a10cd2ed01a2e75c
unknown <simonskylstad@gmail.com>
2025-04-10 14:21:04 +0200

    Updated pom and added the AccessUserDetails class.

---
+++ src/main/java/edu/ntnu/flightbookingbackend/security/AccessUserDetails.java
@@ -0,0 +1,56 @@
+package edu.ntnu.flightbookingbackend.security;
+
+import edu.ntnu.flightbookingbackend.model.User;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AccessUserDetails implements UserDetails {
+  private final User user;
+
+  public AccessUserDetails(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(user.getRole()));
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getEmail(); // Uses email for log in
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public Integer getUserId() {
+    return user.getUserId();
+  }
+}
